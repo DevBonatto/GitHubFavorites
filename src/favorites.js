b61dcefbb8bce61a37655d55b250fe0593bf3e96
DevBonatto <lucca.scaquetti@hotmail.com>
2024-07-04 17:01:49 -0300

    :sparkles: feature: adding a new function

--- src/favorites.js
+++ src/favorites.js
@@ -1,18 +1,15 @@
 import { gitHubUser } from "./GitHubUser.js"
 
-// classe que vai conter a lógica dos dados
-// como os dados serão estruturados
 class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
-
     this.tbody = this.root.querySelector('table tbody')
-
     this.load()
   }
 
   load() {
-    this.users = JSON.parse(localStorage.getItem('@githut-favorites:')) || []
+    const storedData = localStorage.getItem('@githut-favorites:')
+    this.users = storedData ? JSON.parse(storedData) : []
   }
 
   save() {
@@ -23,19 +20,18 @@ class Favorites {
     try {
       const userExists = this.users.find(user => user.login.toLowerCase() === username.toLowerCase())
       if (userExists) {
-        throw new Error('Usuário já existe');
-      }  
+        throw new Error('Usuário já existe')
+      }
 
       const user = await gitHubUser.search(username)
-      if(user.login === undefined) {
+      if (!user.login) {
         throw new Error('Usuário não encontrado')
       }
 
       this.users = [user, ...this.users]
       this.update()
       this.save()
-
-    }catch(error) {
+    } catch (error) {
       alert(error.message)
     }
   }
@@ -48,7 +44,6 @@ class Favorites {
   }
 }
 
-// classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
@@ -58,10 +53,22 @@ export class FavoritesView extends Favorites {
 
   onAdd() {
     const addButton = this.root.querySelector('.search button')
+    const inputField = this.root.querySelector('.search input')
 
     addButton.onclick = () => {
-      const { value } = this.root.querySelector('.search input')
+      const { value } = inputField
       this.add(value)
+    }
+
+    inputField.addEventListener('keypress', (event) => this.handleEnterPress(event))
+  }
+
+  handleEnterPress(event) {
+    const inputField = event.target
+    if(event.key === 'Enter') {
+      const username = inputField.value
+      this.add(username)
+      inputField.value = ''
     }
   }
 
@@ -81,43 +88,40 @@ export class FavoritesView extends Favorites {
 
       row.querySelector('.remove').onclick = () => {
         const isOk = confirm('Tem certeza que deseja deletar esse usuário?')
-        if(isOk) {
+        if (isOk) {
           this.delete(user)
         }
       }
       this.tbody.append(row)
     })
-
   }
 
   createRow() {
     const tr = document.createElement('tr')
 
     tr.innerHTML = `
-          <td class="user">
-            <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
-            <a href="https://github.com/maykbrito" target="_blank">
-              <p>Mayk Brito</p>
-              <span>maykbrito</span>
-            </a>
-          </td>
-          <td class="repositories">
-            <p>22</p>
-          </td>
-          <td class="followers">
-            <p>22</p>
-          </td>
-          <td>
-            <button class="remove">&times;</button>
-          </td>
+      <td class="user">
+        <img src="" alt="">
+        <a href="" target="_blank">
+          <p></p>
+          <span></span>
+        </a>
+      </td>
+      <td class="repositories">
+        <p></p>
+      </td>
+      <td class="followers">
+        <p></p>
+      </td>
+      <td>
+        <button class="remove">&times</button>
+      </td>
     `
+    
     return tr
   }
 
   removeAllTr() {
-    this.tbody.querySelectorAll('tr')
-      .forEach((tr) => {
-        tr.remove()
-      })
+    this.tbody.querySelectorAll('tr').forEach(tr => tr.remove())
   }
 }
